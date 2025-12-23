9aade1dbe8c15ea4c2756ed8cb978f79c0a57e07
rifathossain82 <officialrifat82@gmail.com>
2025-12-23 10:41:08 +0600

    add: onboading model class

---
+++ lib/src/features/onboarding/data/models/onboarding_model.dart
@@ -0,0 +1,53 @@
+import 'package:sme_fin/src/features/onboarding/domain/entities/onboarding_entity.dart';
+
+class OnboardingModel extends OnboardingEntity {
+  const OnboardingModel({
+    required super.email,
+    super.verificationCode,
+    super.fullName,
+    super.phoneNumber,
+    super.businessName,
+    super.tradeLicenseNumber,
+    super.tradeLicensePath,
+    super.isSubmitted,
+  });
+
+  factory OnboardingModel.fromJson(Map<String, dynamic> json) {
+    return OnboardingModel(
+      email: json['email'] as String,
+      verificationCode: json['verificationCode'] as String?,
+      fullName: json['fullName'] as String?,
+      phoneNumber: json['phoneNumber'] as String?,
+      businessName: json['businessName'] as String?,
+      tradeLicenseNumber: json['tradeLicenseNumber'] as String?,
+      tradeLicensePath: json['tradeLicensePath'] as String?,
+      isSubmitted: json['isSubmitted'] as bool? ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'verificationCode': verificationCode,
+      'fullName': fullName,
+      'phoneNumber': phoneNumber,
+      'businessName': businessName,
+      'tradeLicenseNumber': tradeLicenseNumber,
+      'tradeLicensePath': tradeLicensePath,
+      'isSubmitted': isSubmitted,
+    };
+  }
+
+  factory OnboardingModel.fromEntity(OnboardingEntity entity) {
+    return OnboardingModel(
+      email: entity.email,
+      verificationCode: entity.verificationCode,
+      fullName: entity.fullName,
+      phoneNumber: entity.phoneNumber,
+      businessName: entity.businessName,
+      tradeLicenseNumber: entity.tradeLicenseNumber,
+      tradeLicensePath: entity.tradeLicensePath,
+      isSubmitted: entity.isSubmitted,
+    );
+  }
+}
